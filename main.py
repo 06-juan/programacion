47a91a3a82e7ae6120436e546c8ac0ad96975878
06-juan <juanj.gonzalezc1@uqvitual.co>
2025-02-20 06:39:42 -0500

    cambios en main y gitignore

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+'''
 print('Hola mundo')
 nombre= input('¿Como te llamas? ')
 edad = eval(input('¿cual es tu edad? '))
 print(f'Hola! {nombre} sabias que tu edad es {2025-edad}')
+'''
+
+for i,j in enumerate\
+(['juan','sofi','sam']):
+    print(i+1,j)
+
+
+for i,j,k in zip(['juan','sofi','sam'],[100,200,300],['colombia','peru','ecuador']):
+    print(i,j,k)
